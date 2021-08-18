a79f664efe909ffc0c564e5f5ff3924fb683bd3a
Steven Quintana <steve.e.quint@gmail.com>
2021-08-18 14:37:52 -0700

    Delete page load event listener

--- assets/saved-gifs.js
+++ assets/saved-gifs.js
@@ -1,19 +1,14 @@
-window.addEventListener('load', event => {
 
+for (let i = 0; i < localStorage.length; i++) {
+  let key = localStorage.key(i)
+  console.log(key)
+  let gif = localStorage.getItem(key)
 
-
-    for (let i = 0; i < localStorage.length; i++) {
-      let key = localStorage.key(i)
-      console.log(key)
-      let gif = localStorage.getItem(key)
-
-      let divEl = document.createElement('div')
-      divEl.className = 'col-3 card gif'
-      divEl.innerHTML = `
+  let divEl = document.createElement('div')
+  divEl.className = 'col-3 card gif'
+  divEl.innerHTML = `
         <img src=${gif} class="card-img-top" alt="GIF">
         `
-      document.getElementById('saved-display').append(divEl)
+  document.getElementById('saved-display').append(divEl)
 
-    }
-
-})
+}
