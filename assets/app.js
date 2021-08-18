efcefe7549a0f84cf6aeb1b61ba0bb7d956b5d39
Steven Quintana <steve.e.quint@gmail.com>
2021-08-18 11:26:50 -0700

    Update imaageList with two data definitions, data package nested twice

--- assets/app.js
+++ assets/app.js
@@ -4,12 +4,12 @@ document.getElementById('search-btn').addEventListener('click', event => {
   
   axios.get(`https://api.giphy.com/v1/gifs/search?api_key=9zby5Ocx2Cn3yzfMeS7n4QpW9UWQ1npn&limit=20&q=${input}`)
     .then(response => {
-      let imageList = response.data
+      let imageList = response.data.data
       console.log(imageList)
 
       document.getElementById('results').innerHTML = `
       <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="${imageList[0].images.fixed_width_small}" class="card-img-top" alt="GIF">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
