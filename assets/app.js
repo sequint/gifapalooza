46ada7174f3108988134c60d7a38e88278202890
Steven Quintana <steve.e.quint@gmail.com>
2021-08-18 11:21:56 -0700

    Build axois get call to giphy search api

---
+++ assets/app.js
@@ -0,0 +1,23 @@
+document.getElementById('search-btn').addEventListener('click', event => {
+
+  let input = document.getElementById('search-input').value
+  
+  axios.get(`https://api.giphy.com/v1/gifs/search?api_key=9zby5Ocx2Cn3yzfMeS7n4QpW9UWQ1npn&limit=20&q=${input}`)
+    .then(response => {
+      let imageList = response.data
+      console.log(imageList)
+
+      document.getElementById('results').innerHTML = `
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      `
+    })
+    .catch(error => console.log(error))
+
+})
